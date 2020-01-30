888718d32614317ec68a928737c8cd8f0105c052
Alexandra Veres <Alexandra.Veres@cancer.org.uk>
2020-01-30 17:27:10 +0000

    Styling the popup on map.

--- src/components/Map/mapData.js
+++ src/components/Map/mapData.js
@@ -14,11 +14,11 @@ export const mapData = [
     type: "Feature",
     properties: {
       description:
-        "<img width='250px' src='https://images.jtv.com/media/jtv-site/gemopedia/rhodochrosite/rhodochrosite-specimen.jpg'/><div class='caption'><h1>Rhodochrosite</h1></div>"
+        "<img width='250px' src='https://upload.wikimedia.org/wikipedia/commons/0/00/Tourmaline-121240.jpg'/><div class='caption'><h1>Tourmaline</h1></div>"
     },
     geometry: {
       type: "Point",
-      coordinates: [4.84785, 52.360529]
+      coordinates: [139, 35.2552801]
     }
   }
 ];
